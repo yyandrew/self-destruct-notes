ac64c1b67dca14167f35d392da1152797767f320
lq201 <qqerqqer@126.com>
2022-10-04 17:34:37 +0800

    使用golang实现一个简单的笔记示例

--- go.mod
+++ go.mod
@@ -1,3 +1,38 @@
 module github.com/yyandrew/self-destruct-notes
 
 go 1.17
+
+require (
+	github.com/go-kratos/kratos v1.0.1
+	github.com/go-redis/cache/v8 v8.4.3
+	github.com/go-redis/redis/v8 v8.11.5
+	github.com/google/uuid v1.3.0
+)
+
+require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-playground/locales v0.13.0 // indirect
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/prometheus/client_golang v1.11.0 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.26.0 // indirect
+	github.com/prometheus/procfs v0.6.0 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/vmihailenco/go-tinylfu v0.2.2 // indirect
+	github.com/vmihailenco/msgpack/v5 v5.3.4 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
+	golang.org/x/exp v0.0.0-20210916165020-5cb4fee858ee // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	google.golang.org/grpc v1.39.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,26 +1,189 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "os"
+	"fmt"
+	"html/template"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/go-redis/cache/v8"
+	"github.com/go-redis/redis/v8"
+	"github.com/google/uuid"
 )
 
-func main() {
-  port := os.Getenv("PORT")
-  if len(port) == 0 {
-    port = "3000"
-  }
-  addr := ":" + port
-  fmt.Printf("Starting web server, listening on %s\n", addr)
-
-  err := http.ListenAndServe(addr, http.HandlerFunc(webServer))
-  if err != nil {
-    panic(err)
-  }
+type Server struct {
+	BaseURL    string
+	RedisCache *cache.Cache
 }
 
-func webServer(w http.ResponseWriter, r *http.Request) {
-  w.WriteHeader(200)
-  w.Write([]byte("Hello world"))
+type Note struct {
+	Data     []byte
+	Destruct bool
+}
+
+func main() {
+	redisURL1 := os.Getenv("REDIS_URL")
+	if len(redisURL1) == 0 {
+		redisURL1 = "redis://:@localhost:6379/1"
+	}
+	redisOptions, err := redis.ParseURL(redisURL1)
+
+	if err != nil {
+		panic(err)
+	}
+	redisClient := redis.NewClient(redisOptions)
+	defer redisClient.Close()
+
+	redisCache := cache.New(&cache.Options{
+		Redis: redisClient,
+	})
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "3000"
+	}
+	addr := ":" + port
+	fmt.Printf("Starting web server, listening on %s\n", addr)
+
+	baseURL := os.Getenv("BASE_URL")
+	if len(baseURL) == 0 {
+		baseURL = fmt.Sprintf("http://localhost:%s", port)
+
+	}
+
+	server := &Server{
+		BaseURL:    baseURL,
+		RedisCache: redisCache,
+	}
+
+	err = http.ListenAndServe(addr, server)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" || r.Method == "HEAD" {
+		s.handleGET(w, r)
+		return
+	}
+	if r.Method == "POST" && r.URL.Path == "/" {
+		s.handlePOST(w, r)
+		return
+	}
+	s.handleNotFound(w, r)
+}
+
+func (s *Server) handleGET(w http.ResponseWriter, r *http.Request) {
+
+	path := r.URL.Path
+	if path == "/" {
+		s.renderTemplate(w, r, nil, "layout", "dist/layout.html", "dist/index.html")
+		return
+	}
+
+	noteID := strings.TrimPrefix(r.URL.Path, "/")
+	ctx := r.Context()
+	note := &Note{}
+	err := s.RedisCache.GetSkippingLocalCache(
+		ctx,
+		noteID,
+		note,
+	)
+	if err != nil {
+		s.badRequest(w, r, http.StatusNotFound, fmt.Sprintf("You requested the note with the ID: '%s'.", noteID))
+		return
+	}
+	if note.Destruct {
+		err := s.RedisCache.Delete(ctx, noteID)
+		if err != nil {
+			fmt.Println(err)
+			s.serverError(w, r)
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(note.Data)
+}
+
+func (s *Server) handlePOST(w http.ResponseWriter, r *http.Request) {
+	mediaType := r.Header.Get("Content-Type")
+	if mediaType != "application/x-www-form-urlencoded" {
+		s.badRequest(w, r, http.StatusUnsupportedMediaType, "Invalid media type posted.")
+		return
+	}
+	err := r.ParseForm()
+	if err != nil {
+		s.badRequest(w, r, http.StatusBadRequest, "Invalid form data posted")
+	}
+	form := r.PostForm
+
+	message := form.Get("message")
+	destruct := false
+	ttl := time.Hour * 24
+	if form.Get("ttl") == "untilRead" {
+		destruct = true
+		ttl = ttl * 365
+	}
+
+	note := &Note{
+		Data:     []byte(message),
+		Destruct: destruct,
+	}
+
+	key := uuid.NewString()
+	err = s.RedisCache.Set(
+		&cache.Item{
+			Ctx:            r.Context(),
+			Key:            key,
+			Value:          note,
+			TTL:            ttl,
+			SkipLocalCache: true,
+		},
+	)
+
+	if err != nil {
+		fmt.Println(err)
+		s.serverError(w, r)
+		return
+	}
+	noteURL := fmt.Sprintf("%s/%s", s.BaseURL, key)
+	w.WriteHeader(http.StatusOK)
+	// fmt.Printf("<a href='%s'>%s</a>", noteURL, noteURL)
+	s.renderMessage(w, r, "Note was successfully created", template.HTML(fmt.Sprintf("<a href='%s'>%s</a>", noteURL, noteURL)))
+}
+
+func (s *Server) handleNotFound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte("Not Found"))
+}
+
+func (s *Server) renderTemplate(w http.ResponseWriter, r *http.Request, data interface{}, name string, files ...string) {
+	t := template.Must(template.ParseFiles(files...))
+
+	err := t.ExecuteTemplate(w, name, data)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (s *Server) badRequest(w http.ResponseWriter, r *http.Request, statusCode int, message string) {
+	w.WriteHeader(statusCode)
+	w.Write([]byte(message))
+}
+
+func (s *Server) serverError(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write([]byte("Ops something went wrong. Please check the server logs."))
+}
+
+func (s *Server) renderMessage(w http.ResponseWriter, r *http.Request, title string, paragraphs ...interface{}) {
+	fmt.Printf("titls: %s", title)
+	fmt.Printf("paragraphs: %+v", paragraphs)
+	s.renderTemplate(w, r, struct {
+		Title      string
+		Paragraphs []interface{}
+	}{Title: title, Paragraphs: paragraphs}, "layout", "dist/layout.html", "dist/message.html")
 }
